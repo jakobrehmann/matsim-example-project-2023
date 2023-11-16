3236647b5dc0119eaf3f6118ddd1188e9fe0582d
Jakob Rehmann <jakob.rehmann@gmail.com>
2023-11-16 10:40:20 +0100

    add test

---
+++ src/test/java/org/matsim/project/jakobTest.java
@@ -0,0 +1,18 @@
+package org.matsim.project;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class jakobTest {
+
+
+    @Test
+    public void testytest(){
+
+
+        double x = 2 + 3;
+
+        Assert.assertEquals(5., x, 0.01);
+
+    }
+}
